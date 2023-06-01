9d371ae0749387bab5809ce0045a5ca8a55ef4be
zStynx <diegoalonso140227@gmail.com>
2023-06-01 18:07:43 -0500

    DJANGO SUBIDO

---
+++ Proyecto001/miapp/views.py
@@ -0,0 +1,68 @@
+from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, HttpResponse, redirect
+# Create your views here.
+layout = """
+<h1> Proyecto Web (LP3) | Flor Cerdán </h1>
+<hr/>
+<ul>
+<li>
+<a href="/inicio"> Inicio</a>
+</li>
+<li>
+<a href="/saludo"> Mensaje de Saludo</a>
+</li>
+<li>
+<a href="/rango"> Mostrar Números [a,b]</a>
+</li>
+<li>
+<a href="/rango2/10/15"> Mostrar Números [10,15]</a>
+</li>
+</ul>
+"""
+
+def index(request):
+    mensaje="""
+    <h1>Inicio</h1>
+    """
+    return HttpResponse(layout + mensaje)
+def saludo(request):
+    mensaje = """
+    <h1>Bienvenidos</h1>
+    <h2>Flor Cerdán</h2>
+    <h3>Python....</h3>
+    """
+    return HttpResponse(layout + mensaje)
+def rango(request):
+    a = 10
+    b = 20
+    resultado = f"""
+    <h2> Números de [{a},{b}] </h2>
+    Resultado: <br>
+    <ul>
+    """
+
+    while a<=b:
+        resultado += f"<li> {a} </li>"
+        a+=1
+    resultado += "</ul"
+    return HttpResponse(layout + resultado)
+def rango2(request,a=0,b=100):
+    resultado = f"""
+    <h2> Números de [{a},{b}] </h2>
+    Resultado: <br>
+    <ul>
+    """
+    while a<=b:
+        resultado += f"<li> {a} </li>"
+        a+=1
+    resultado += "</ul"
+    return HttpResponse(layout + resultado)
+
+
+
+def rango2(request,a=0,b=100):
+    if a>b:
+        return redirect('rango2',a=b, b=a)
+    resultado = f"""
+    <h2> Números de [{a},{b}] </h2>
+    """
